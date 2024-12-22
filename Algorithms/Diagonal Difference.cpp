e44a6db98ea8363734ffb962488ef3d854c0f016
Devansh Patel <128317150+Devansh-Patel-2929@users.noreply.github.com>
2024-12-23 03:19:32 +0530

    Create Diagonal Diffe

---
+++ Algorithms/Diagonal Difference.cpp
@@ -0,0 +1,13 @@
+int diagonalDifference(vector<vector<int>> arr) {
+    int TL = 0; 
+    int TR = 0;
+    int n = arr.size();
+    
+    for(int i = 0; i < n; i++) {
+        TL += arr[i][i];
+        TR += arr[i][n - i - 1];
+    }
+    
+    int diff = abs(TL - TR);
+    return diff;
+}
